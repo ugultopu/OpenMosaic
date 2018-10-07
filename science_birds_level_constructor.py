ad6a4b3b570ea7075e6ea4fdebe14903e0409cc2
Utku Gultopu <ugultopu@gmail.com>
2018-10-07 19:33:19 -0300

    Create functions to get a list of platforms

--- science_birds_level_constructor.py
+++ science_birds_level_constructor.py
@@ -1,4 +1,9 @@
+from random import randint
+
 LENGTH_OF_SQUARE_BLOCK_EDGE = 0.43
+WIDTH_OF_RECTANGLE = 2.06
+HEIGHT_OF_RECTANGLE = 0.22
+NUMBER_OF_TILES_PER_EDGE_OF_SQUARE_PLATFORM = 5
 Y_COORDINATE_OF_GROUND = -3.5
 
 
@@ -25,6 +30,29 @@ def remove_ice_blocks(mosaic_tiles):
     remove them.
     """
     return [[tile for tile in column if get_block_type(tile) != 'ice'] for column in mosaic_tiles]
+
+
+def platform_intersects_with_existing_platform(new_platform_coordinate, platform_start_coordinates):
+    for coordinates in new_platform_coordinate:
+        if abs(coordinates[0] - new_platform_coordinate[0]) < NUMBER_OF_TILES_PER_EDGE_OF_SQUARE_PLATFORM and abs(coordinates[1] - new_platform_coordinate[1]) < NUMBER_OF_TILES_PER_EDGE_OF_SQUARE_PLATFORM:
+            return True
+    return False
+
+
+def get_platform_coordinates(mosaic_tiles):
+    """
+    Returns a list of coordinates that indicate the starting point of the
+    platforms.
+    """
+    number_of_unsuccessful_attempts_to_get_a_coordinate = 0
+    coordinates = []
+    while number_of_unsuccessful_attempts_to_get_a_coordinate < 3:
+        coordinate = (randint(0, len(mosaic_tiles) - 1 - NUMBER_OF_TILES_PER_EDGE_OF_SQUARE_PLATFORM), randint(NUMBER_OF_TILES_PER_EDGE_OF_SQUARE_PLATFORM - 1, len(mosaic_tiles[column_index]) - 1))
+        if not platform_overlap_with_existing_platform(coordinate, coordinates):
+            coordinates.append(coordinate)
+        else:
+            number_of_unsuccessful_attempts_to_get_a_coordinate += 1
+    return coordinates
 
 
 def get_xml_elements_from_mosaic(mosaic_tiles):
